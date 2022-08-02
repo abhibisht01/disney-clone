7f3de8fe6218edce5663c4633a172192c93f8a74
Abhishek Bisht <99400134+abhibisht01@users.noreply.github.com>
2022-08-02 16:29:17 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 const firebaseConfig = {
-    apiKey: "AIzaSyDGZWeN49tuLpCrgDlhBinPkMUgEBvztQ0",
-    authDomain: "disney-clone-9c479.firebaseapp.com",
-    projectId: "disney-clone-9c479",
-    storageBucket: "disney-clone-9c479.appspot.com",
-    messagingSenderId: "773111862473",
-    appId: "1:773111862473:web:631a0a1ea2c078d33ee918",
-    measurementId: "G-3JX8W85JB6"
+   add Yours
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
